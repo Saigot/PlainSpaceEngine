fb6555e489478b16a4cb9d22b4554a2c2fdea03b
Saigot <mgscovell@gmail.com>
2014-05-16 13:16:34 -0400

    Allowed for finite planes, gave camera the plane variable

--- src/plainspaceengine/Plane.java
+++ src/plainspaceengine/Plane.java
@@ -14,4 +14,18 @@ public class Plane {
     Vec norm;
     boolean normDefined;
     boolean vectorDefined;
+    
+    /**All finite planes must be vectordefined. 
+     * A finite plane is defined as:
+     * plane = a*vec1 + b*vec2 
+     * where scalarmin1 <= a <= scalarmax1
+     * with the same case for b
+     * if either of the scalar maxima are 0, they will be treated as unbounded.
+     * for a finite plane, at least one of the scalar maxima for each vector must be defined.
+     */
+    boolean finite;
+    float scalarmax1;
+    float scalarmax2;
+    float scalarmin1;
+    float scalarmin2;
 }
