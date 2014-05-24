274b6986af2deea16b629a777a583a08653df9f1
Saigot <mgscovell@gmail.com>
2014-05-24 00:39:00 -0400

    a few empty subroutines added for later use

--- src/plainspaceengine/Shapes/Polyhedron.java
+++ src/plainspaceengine/Shapes/Polyhedron.java
@@ -1,6 +1,7 @@
 
 package plainspaceengine.Shapes;
 
+import java.util.ArrayList;
 import plainspaceengine.Point;
 
 /**
@@ -12,11 +13,19 @@ import plainspaceengine.Point;
  */
 public class Polyhedron {
     Point Anchor;
-    
+    ArrayList<Vertex> vert = new ArrayList<>();
     //Moves the Anchor Relative to the vertexes, useful for changing the point being rotated from
     public void MoveAnchor(Point newLocation){
     }
     public void MoveTo(Point newLocation){
         Anchor = newLocation;
+    }
+    
+    public Polyhedron Merge(Polyhedron toAdd, int mergedPts1[], int mergedPts2[] ){
+        return null;
+    }
+    
+    public void addPoint(Point p, int... connections){
+        
     }
 }
