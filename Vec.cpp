69b19d76f6eb14321b17ccb68a832c4f7714fe64
Saigot <mgscovell@gmail.com>
2014-06-04 12:22:35 -0400

    basic commenting on functions

--- Vec.cpp
+++ Vec.cpp
@@ -59,10 +59,13 @@ Vec::Vec(float x, float y , float z, bool twoD){
         anchored = true;
 }*/
 
+//Addition operator for 2 vectors, returns a new vector
 Vec Vec::operator+(const Vec  &vec) {
     Vec ret(x + vec.x, y + vec.y, z + vec.z);
     return ret;
 }
+
+//Creates a new Vector with each element of this vector negated
 Vec Vec::operator-(){
   //Vec v(*this);
   Vec v;
@@ -72,10 +75,12 @@ Vec Vec::operator-(){
   return v;
 }
 
+//preforms the standard inner product between these two vecs 
 float Vec::operator*(const Vec  &vec) {
     return x*vec.x + y*vec.y + z*vec.z;
 }
 
+//Checks if two vectors are equivilent
 bool Vec::operator==(const Vec  &vec){
     bool ret = x == vec.x && y == vec.y;
     if(twoD != vec.twoD) return false;
@@ -84,6 +89,7 @@ bool Vec::operator==(const Vec  &vec){
 //    if(anchored) ret = ret && anchor == vec.anchor;
 }
 
+//Mutates this object, adds this vector to vec
 void Vec::operator+=(const Vec  &vec) {
     Vec ret(x + vec.x, y + vec.y, z + vec.z);
     x+=vec.x;
@@ -91,37 +97,44 @@ void Vec::operator+=(const Vec  &vec) {
     z+=vec.z;
 }
 
-
-
-
+//subtracts two vectors, returns a new vector
 Vec Vec::operator-(const Vec  &vec) {
     Vec ret(x - vec.x, y - vec.y, z - vec.z);
     return ret;
 }
 
+//the cross product of this and v, returns anew vector
  Vec Vec::cross(Vec &v){
         Vec ret = Vec(y * v.z - v.y*z,
                 z * v.x - x * v.z, 
                 x * v.y - v.x * y);
         return ret;
 }
- 
+
+//returns a vector that is scaled, must be of the form vec*scalar
 Vec Vec::operator*(float s){
     return Vec(x*s, y*s, z*s);
 }
 
+//equivalent to this = this*scalar
 void Vec::operator*=(float s){
     x*=s;
     y*=s;
     z*=s;
 }
 
+//returns a new vector that is the result of a vec projected onto this
 Vec Vec::proj(Vec vec){
-        float constant = (vec**this) / (*this**this);
+        float constant = (vec * *this) / (*this * *this);
         Vec ret;
+        ret.x = vec.x;
+        ret.y = vec.y;
+        ret.z = vec.z;
         ret*=constant;
         return ret;
 }
+
+//Returns a new vector that is the result of perp of vec onto this
 Vec Vec::perp(Vec vec){
     return vec - proj(vec);
 }
